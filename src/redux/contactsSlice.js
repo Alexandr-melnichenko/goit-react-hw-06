a93656d7b183f896d75edd6ae731da6e397786e2
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2024-11-30 23:45:21 +0200

    ready slices

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  contacts: {
+    items: [],
+  },
+  filters: {
+    name: "",
+  },
+};
+
+const slice = createSlice({
+  name: "contacts",
+  initialState,
+  reducers: {
+    addContact: (state, action) => {
+      state.items.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      state.contacts = state.items.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const selectContacts = (state) => state.contacts.items;
+export const contactsReducer = slice.reducer;
+export const { addContact, deleteContact } = slice.actions;
+
+// Функція добавлення нового контакту до списку
+// const addContact = (newContact) => {
+//   setContact((prevContacts) => [...prevContacts, newContact]);
+// };
+
+// Функція видалення контакту
+// const deleteContact = (id) => {
+//   setContact((prevContacts) =>
+//     prevContacts.filter((contact) => contact.id !== id)
+//   );
+// };
---
+++ src/redux/filtersSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import searchValue from "../components/App";
+
+const initialState = {
+  contacts: {
+    items: [],
+  },
+  filters: {
+    name: "",
+  },
+};
+
+const slice = createSlice({
+  name: "filters",
+  initialState,
+  reducers: {
+    changeFilter: (state, action) => {
+      state.filters = state.filters.name
+        .toLowerCase()
+        .includes(searchValue.toLowerCase(action.payload));
+    },
+  },
+});
+
+export const filtersReducer = slice.reducer;
+export const selectNameFilter = (state) => state.filters.name;
+export const { changeFilter } = slice.actions;
+
+//Фільтрований список контактів
+// const filteredContacts = contacts.filter((contact) =>
+//     contact.name.toLowerCase().includes(searchValue.toLowerCase())
+//   );
